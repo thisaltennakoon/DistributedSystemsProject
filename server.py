5f29fd580f6063c9d69c22a835171a22c7bf8688
Thisal Tennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2021-09-12 21:42:46 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ import json
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+clients = []
+
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
@@ -15,12 +17,18 @@ while True:
             #print('Connected by', addr)
             while True:
                 data = conn.recv(1024)
-                print('Client : ',data)
-
-                if data == b'{"identity":"Adel","type":"newidentity"}\n':
-                    reply1 = json.dumps({"type" : "newidentity", "approved" : "true"}, ensure_ascii=False).encode('utf8')+'\n'.encode('utf8')
-                    reply2 = json.dumps({"type" : "roomchange", "identity" : "Adel", "former" : "", "roomid" : "MainHall-s1"}, ensure_ascii=False).encode('utf8') + '\n'.encode('utf8')
-                    conn.sendall(reply1+reply2)
+                #print('Client : ',data)
+                #print(type(data))
+                data = json.loads(data.decode("utf-8"))
+                print(data)
+                if data['type'] == 'newidentity':
+                    if data['identity'] not in clients:
+                        clients.append(data['identity'])
+                        reply1 = json.dumps({"type" : "newidentity", "approved" : "true"}, ensure_ascii=False).encode('utf8')+'\n'.encode('utf8')
+                        reply2 = json.dumps({"type" : "roomchange", "identity" : "Adel", "former" : "", "roomid" : "MainHall-s1"}, ensure_ascii=False).encode('utf8') + '\n'.encode('utf8')
+                        conn.sendall(reply1+reply2)
+                    else:
+                        conn.sendall(json.dumps({"type" : "newidentity", "approved" : "false"}, ensure_ascii=False).encode('utf8')+'\n'.encode('utf8'))
                 elif data == b'{"type" : "list"}\n':
                     print('list')
                 elif data == b'{"type" : "who"}\n':
@@ -29,5 +37,6 @@ while True:
                     break
 
 
-#python echo-server.py
+#python server.py
+#java -jar client.jar -h 127.0.0.1 -p 65432 -i Adel -d
 
